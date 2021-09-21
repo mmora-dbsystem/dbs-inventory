c25f5fb6589359f7eb92d55fa7ac10cbfbe164e1
Miguel Mora <mmora@db-system.com>
2021-09-21 14:04:15 -0500

    ambientes

--- src/keys.js
+++ src/keys.js
@@ -5,20 +5,23 @@ module.exports = {
     database: {
         //Hostname o IP del servidor de base de datos...
         //Desarrollo 
-        //host: 'localhost',
+        //host: '192.168.100.12',
         //Produccion
         host: '192.168.100.12',
         //Nombre de usuario para la conexion de BD...
         //Desarrollo
-        //user: 'root',
+        //user: 'dbsinventory',
         //Produccion
         user: 'dbsinventory',
         //Contraseña del usuario de BD...
         //Desarrollo
-        //password: '',
+        //password: 'dbsinventory123',
         //Produccion
         password: 'dbsinventory123*',
         //Nombre de la BD...
+        //Desarrollo:
+        //database: 'dbs_inventory_dev'
+        //Produccion
         database: 'dbs_inventory'
     }
 };
